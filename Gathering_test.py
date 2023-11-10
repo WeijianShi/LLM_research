a290053f52d815e6d47c7acbaf25b0cd4165fc34
WeijianShi <shiweijian0818@gmail.com>
2023-11-09 22:45:19 -0500

    Llamma2 method update

--- Gathering_test.py
+++ Gathering_test.py
@@ -7,6 +7,7 @@ from transformers import AutoTokenizer
 import transformers
 import torch
 import langchain
+from langchain import HuggingFacePipeline, LLMChain
 
 from parameters import LLM_model 
 
@@ -19,14 +20,19 @@ pipeline = transformers.pipeline(
     "text-generation", 
     model=LLM_model,
     tokenizer=tokenizer,
-    # torch_dtype=torch.bfloat16,
+    torch_dtype=torch.float16,
     trust_remote_code=True,
     device_map="auto",
     max_length=1000,
     eos_token_id=tokenizer.eos_token_id
 )
 
-llm = langchain.llms.huggingface_pipeline.HuggingFacePipeline(pipeline = pipeline, model_kwargs = {'temperature':0})
+print("pipeline setup finished")
+
+llm = HuggingFacePipeline(pipeline = pipeline, model_kwargs = {'temperature':0})
+
+print("llm setup finished")
+
 template = """
               You are an intelligent chatbot that gives out useful information to humans.
               You return the responses in sentences with arrows at the start of each sentence
@@ -35,7 +41,7 @@ template = """
 
 prompt = langchain.prompts.PromptTemplate(template=template, input_variables=["query"])
 
-llm_chain = langchain.chains.LLMChain(prompt=prompt, llm=llm)
+llm_chain = LLMChain(prompt=prompt, llm=llm)
 print(llm_chain.run('What are the 3 causes of glacier meltdowns?'))
 
 
